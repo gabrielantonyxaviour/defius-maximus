e72b1ea7ce86eb2d21d8ef189ae27d0f6a369ec9
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2025-02-26 04:15:53 -0700

    wohoo

--- test-debridge/index.ts
+++ test-debridge/index.ts
@@ -1,7 +1,11 @@
-import { evm } from "@debridge-finance/desdk";
-import { Flags } from "@debridge-finance/desdk/lib/evm";
-import { createPublicClient, createWalletClient, http } from "viem";
-import { privateKeyToAccount } from "viem/accounts";
+import {
+  createPublicClient,
+  createWalletClient,
+  Hex,
+  http,
+  zeroAddress,
+} from "viem";
+import { privateKeyToAccount, privateKeyToAddress } from "viem/accounts";
 import { base } from "viem/chains";
 import dotenv from "dotenv";
 import { DEBRIDGE_GATE_ABI } from "./config";
@@ -9,46 +13,84 @@ dotenv.config();
 
 async function main() {
   console.log("Starting deBridge transfer process...");
+  const DEFIUS_PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
+  const defiusAddress = privateKeyToAddress(
+    DEFIUS_PRIVATE_KEY as `0x${string}`
+  );
+  console.log(defiusAddress);
   try {
     // Load private key and create account
-    // console.log("Creating wallet account...");
-    // const account = privateKeyToAccount(
-    //   process.env.MAINNET_PRIVATE_KEY as `0x${string}`
-    // );
-    // console.log(`Account address: ${account.address}`);
+    console.log("Creating wallet account...");
+    const account = privateKeyToAccount(
+      process.env.MAINNET_PRIVATE_KEY as `0x${string}`
+    );
+    console.log(`Account address: ${account.address}`);
 
-    // // Initialize wallet client
-    // console.log("Initializing wallet client on Base network...");
-    // const walletClient = createWalletClient({
-    //   account,
-    //   chain: base,
-    //   transport: http(
-    //     "https://base-mainnet.g.alchemy.com/v2/" + process.env.ALCHMEY_API_KEY
-    //   ),
-    // });
+    // Initialize wallet client
+    console.log("Initializing wallet client on Base network...");
+    const walletClient = createWalletClient({
+      account,
+      chain: base,
+      transport: http(
+        "https://base-mainnet.g.alchemy.com/v2/" + process.env.ALCHMEY_API_KEY
+      ),
+    });
 
     // // Initialize public client
-    // console.log("Initializing public client...");
-    // const publicClient = createPublicClient({
-    //   chain: base,
-    //   transport: http(
-    //     "https://base-mainnet.g.alchemy.com/v2/" + process.env.ALCHMEY_API_KEY
-    //   ),
-    // });
+    console.log("Initializing public client...");
+    const publicClient = createPublicClient({
+      chain: base,
+      transport: http(
+        "https://base-mainnet.g.alchemy.com/v2/" + process.env.ALCHMEY_API_KEY
+      ),
+    });
 
     // // Set receiver address (destination address on Arbitrum)
-    // const receiver = "0x91DdF5c1684905702Ef33459Dd2806f10cac78C1";
-    // console.log(`Receiver address: ${receiver}`);
-    // console.log(`Destination chain ID: 42161 (Arbitrum)`);
+    const receiver = "0x91DdF5c1684905702Ef33459Dd2806f10cac78C1";
+    console.log(`Receiver address: ${receiver}`);
+    console.log(`Destination chain ID: 42161 (Arbitrum)`);
 
     // // Native token amount to transfer (in wei)
-    // const transferAmount = "10000000000000"; // 0.0001 ETH
-    // console.log(
-    //   `Amount to transfer: ${transferAmount} wei (${
-    //     Number(transferAmount) / 1e18
-    //   } ETH)`
-    // );
+    const transferAmount = "10000000000000"; // 0.0001 ETH
+    console.log(
+      `Amount to transfer: ${transferAmount} wei (${
+        Number(transferAmount) / 1e18
+      } ETH)`
+    );
+
+    const sourceChain = "8453";
+    const destinationChain = "42161";
+    const amount = "1000000000000000"; // 0.001
+
+    const dlnSwapEndpoint = `https://dln.debridge.finance/v1.0/dln/order/create-tx?srcChainId=${sourceChain}&srcChainTokenIn=${"0x4200000000000000000000000000000000000006"}&srcChainTokenInAmount=${amount}&dstChainId=${destinationChain}&dstChainTokenOut=${zeroAddress}&dstChainTokenOutAmount=auto&dstChainTokenOutRecipient=${
+      account.address
+    }&senderAddress=${account.address}&srcChainOrderAuthorityAddress=${
+      account.address
+    }&affiliateFeePercent=0&dstChainOrderAuthorityAddress=${
+      account.address
+    }&prependOperatingExpenses=false&skipSolanaRecipientValidation=false`;
+
+    console.log(dlnSwapEndpoint);
+    const response = await fetch(dlnSwapEndpoint);
+    const responseData = await response.json();
+    const tx: {
+      to: Hex;
+      data: Hex;
+      value: string;
+    } = responseData.tx;
+    console.log("RECIVED TX");
+    console.log(tx);
+
+    const txHash = await walletClient.sendTransaction({
+      to: tx.to,
+      data: tx.data,
+      value: BigInt(tx.value),
+    });
+
+    console.log(txHash);
+
+    // console.log(txHash);
 
     // // Create message object
     // console.log("Creating deBridge message...");
@@ -104,59 +146,6 @@ async function main() {
     // const tx = await walletClient.writeContract(request);
     // console.log(`Transaction sent! Hash: ${tx}`);
     // console.log(`View on explorer: https://basescan.org/tx/${tx}`);
-
-    const evmOriginContext: evm.Context = {
-      // provide a URL to the RPC node of the 🛫origin chain
-      provider:
-        "https://base-mainnet.g.alchemy.com/v2/" + process.env.ALCHMEY_API_KEY,
-    };
-
-    const evmDestinationContext: evm.Context = {
-      // provide a URL to the RPC node of the 🛬destination chain
-      provider:
-        "https://arb-mainnet.g.alchemy.com/v2/" + process.env.ALCHMEY_API_KEY,
-    };
-
-    const submissions = await evm.Submission.findAll(
-      "0xf982851e97ffbed7dc906676738567ef08966d71f618a8c343fb250ab05f7381",
-      evmOriginContext
-    );
-
-    const [submission] = submissions;
-
-    // check if submission if confirmed: validator nodes wait a specific block
-    // confirmations before sign the message. Currently, 12 blocks is expected
-    // for most supported EVM chains (256 for Polygon).
-    const isConfirmed = await submission.hasRequiredBlockConfirmations();
-
-    // there is also a bunch of useful properties that describe the submission, e.g.
-    console.log("cross-chain asset ID transferred: ", submission.debridgeId);
-    console.log(
-      "amount transferred to",
-      submission.amount,
-      submission.receiver
-    );
-
-    if (isConfirmed) {
-      const claim = await submission.toEVMClaim(evmDestinationContext);
-
-      // check if claim has been signed by enough validators
-      const isSigned = await claim.isSigned();
-
-      // check if this claim has been already executed
-      const isExecuted = await claim.isExecuted();
-
-      console.log("Claim is signed: ", isSigned);
-      console.log("Claim is executed: ", isExecuted);
-      // get claim args
-      if (isSigned && !isExecuted) {
-        // the resulting tuple of args to be then passed to the deBridgeGate.claim() method
-        const claimArgs = await claim.getEncodedArgs();
-
-        // e.g. using ethers.js:
-        // await deBridgeGate.claim(...claimArgs, { gasLimit: 8_000_000 });
-      }
-    }
 
     // return tx;
   } catch (error) {
