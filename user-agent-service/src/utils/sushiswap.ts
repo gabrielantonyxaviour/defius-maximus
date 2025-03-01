3a728a28f74e84e9e9780b7959fda24d3b0dfd82
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2025-03-01 08:08:50 -0700

    sushi is ready

--- user-agent-service/src/utils/sushiswap.ts
+++ user-agent-service/src/utils/sushiswap.ts
@@ -1,7 +1,7 @@
 import { getSwap, ChainId } from "sushi";
 import { createPublicClient, createWalletClient, http, type Hex } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
-import { rootstock } from "viem/chains";
+import { base } from "viem/chains";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -21,16 +21,14 @@ export async function performSwap({
   console.log(account.address);
 
   const publicClient = createPublicClient({
-    chain: rootstock,
+    chain: base,
     transport: http(
-      "https://rootstock-mainnet.g.alchemy.com/v2/" +
-        process.env.ALCHMEY_API_KEY
+      "https://base-mainnet.g.alchemy.com/v2/" + process.env.ALCHMEY_API_KEY
     ),
   });
 
-  // Get a swap from the API
   const data = await getSwap({
-    chainId: ChainId.ROOTSTOCK, // ethereum chain id
+    chainId: ChainId.BASE, // ethereum chain id
     tokenIn: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE" as Hex,
     tokenOut: tokenOut as Hex,
     to: account.address,
@@ -40,30 +38,25 @@ export async function performSwap({
   });
   console.log(data);
 
-  // If the swap status is 'Success'
   if (data.status === "Success") {
     const { tx } = data;
-    // Simulate a call to the blockchain for the swap
     const callResult = await publicClient.call({
       account: tx.from,
       data: tx.data,
       to: tx.to,
       value: tx.value,
     });
-    // Returns the simulated amount out
     console.log("Output: ", callResult);
 
-    // Send a transaction
     const walletClient = createWalletClient({
-      chain: rootstock,
+      chain: base,
       transport: http(
-        "https://rootstock-mainnet.g.alchemy.com/v2/" +
-          process.env.ALCHMEY_API_KEY
+        "https://base-mainnet.g.alchemy.com/v2/" + process.env.ALCHMEY_API_KEY
       ),
     });
 
     const hash = await walletClient.sendTransaction({
-      chain: rootstock,
+      chain: base,
       account,
       data: tx.data,
       to: tx.to,
